e64c26a0e785f67c1499fbe8621972b67f614c0f
zhouyq <zyq916@gmail.com>
2016-05-19 12:37:01 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,10 +2,20 @@
 
 [ $DEBUG ] && set -x
 
-
 HOST_ID=${POD_ORDER:4}
+
+source /tmp/bin/set_config_file.sh
+
 
 if [ ! -d $ZK_DATA/$HOST_ID ];then
  mkdir -pv ${ZK_DATA}/${HOST_ID} 
  chown zookeeper.zookeeper ${ZK_DATA}/${HOST_ID} -R
 fi
+
+# allow the container to be started with `--user`
+if [ "$(id -u)" = '0' ]; then
+	chown -R zookeeper .
+	exec su-exec zookeeper /opt/zookeeper/bin/zkServer.sh "$@"
+fi
+
+exec /opt/zookeeper/bin/zkServer.sh "$@"
