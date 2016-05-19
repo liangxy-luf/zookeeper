dd12f77f71142a19f2cab701baf338f0ded74cd8
zhouyq <zyq916@gmail.com>
2016-05-19 23:58:07 +0800

    add cluster discovrery

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,9 +5,41 @@
 HOST_ID=${POD_ORDER:4}
 ((HOST_ID++))
 
+LOG_LEVEL=${LOG_LEVEL:-1}
+
+
 if [ ! -d $ZK_DATA/$HOST_ID ];then
  mkdir -pv ${ZK_DATA}/${HOST_ID} 
  chown zookeeper.zookeeper ${ZK_DATA}/${HOST_ID} -R
+fi
+
+# wait zookeeper
+MAXWAIT=${MAXWAIT:-30}
+wait=0
+while [ $wait -lt $MAXWAIT ]
+do
+    echo stat | nc $ZK_HOST $ZK_PORT
+    if ! grep SERVER_IP $ZK_CFG;then
+        break
+    fi
+    wait=`expr $wait + 1`;
+    echo "Waiting cluster other zookeeper service $wait seconds"
+    
+    NodeNetPlugin -url=http://172.30.42.1:8080/api/v1/namespaces/${TENANT_ID}/endpoints/ \
+    -regx_label=${SERVICE_NAME} \
+    -frequency=once \
+    -exec_num=3 \
+    -interval=10 \
+    -regx_port=2181 \
+    -v=${LOG_LEVEL} \
+    -logtostderr=true \
+    -rec_cmd=/opt/zookeeper/bin/set_config.sh
+    sleep 1
+done
+
+if [ "$wait" = $MAXWAIT ]; then
+	echo >&2 'zookeeper cluster start failed.'
+	exit 1
 fi
 
 # allow the container to be started with `--user`
