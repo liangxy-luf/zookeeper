6b74af7c158220046a78af930ee4d73dfe5de19f
zhouyq <zyq916@gmail.com>
2016-06-08 17:39:58 +0800

    Update set_config.sh

--- bin/set_config.sh
+++ bin/set_config.sh
@@ -11,7 +11,7 @@ if [ "${#NODE_LIST[*]}" == "$SERVICE_POD_NUM" ];then
     zk_port=${node[1]}
     zk_order=${node[2]}
     ((zk_order++))
-    echo server.$zk_order=$zk_ip:2888:3888 >> ZK_CFG
+    echo server.$zk_order=$zk_ip:2888:3888 >> $ZK_CFG
   done
   
   touch /tmp/cluster_ok
