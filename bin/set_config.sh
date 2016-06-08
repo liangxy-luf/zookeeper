5d6dc354c37aba179ca6ab84261cb3438f345467
zhouyq <zyq916@gmail.com>
2016-06-08 15:51:54 +0800

    Update set_config.sh

--- bin/set_config.sh
+++ bin/set_config.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 
-HOST_LIST=$1
+NODE_LIST=(`echo $1 | awk -F ',' '{print $1,$2,$3}'`)
 
 [ ! $HOST_LIST ] && exit 0
 
-OLD_IFS=$IFS
-IFS=","
-
-for INFO in $HOST_LIST
-do
-  IFS=" "
-  node=(`echo $INFO | awk -F ':' '{print $1,$2,$3}'`)
-  zk_ip=${node[0]}
-  zk_port=${node[1]}
-  zk_order=${node[2]}
-  ((zk_order++))
-  echo server.$zk_order=$zk_ip:2888:3888 >> ZK_CFG
-done
-
-IFS=$OLD_IFS
+if [ "${#HOST_LIST[*]}"" == "$SERVICE_POD_NUM" ];then
+  for INFO in $HOST_LIST
+  do
+    IFS=" "
+    node=(`echo $INFO | awk -F ':' '{print $1,$2,$3}'`)
+    zk_ip=${node[0]}
+    zk_port=${node[1]}
+    zk_order=${node[2]}
+    ((zk_order++))
+    echo server.$zk_order=$zk_ip:2888:3888 >> ZK_CFG
+  done
+  
+  touch /tmp/cluster_ok
+fi
